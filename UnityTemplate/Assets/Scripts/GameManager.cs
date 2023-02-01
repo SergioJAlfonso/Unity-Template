2235fa2b033d375dca544802937558a7e97b58ca
Laura <lgomez25@ucm.es>
2023-02-01 12:56:31 +0100

    GameManager

---
+++ UnityTemplate/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        //Cosa que viene en los apuntes para que el gamemanager no se destruya entre escenas
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
